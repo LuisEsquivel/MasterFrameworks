1f2dc7a53b3aed5a0ccfd49fbca020a7bc76a1b0
LuisEsquivel <32809166+LuisEsquivel@users.noreply.github.com>
2020-09-13 21:23:17 -0500

    CAMBIOS EN CONTROLLER DE BAKEND
    SE AGREGÓ EL METODO PARA
    ACTUALIZAR ARTICULO
    ELIMINAR ARTICULO
    GUARDAR IMAGEN
    OBTENER IMAGEN

--- Backend/controllers/article.js
+++ Backend/controllers/article.js
@@ -7,6 +7,8 @@
 var validator = require('validator');
 var Article = require('../models/article');
 const article = require('../models/article');
+var fs = require('fs');
+var path = require('path');
 
 var controller = {
 
@@ -132,7 +134,200 @@ var controller = {
     });//end query.sort
 
 
+    },
+
+
+    updateArticle: (req, res) => {
+
+        try {
+            var params = req.body;
+            var validatorTitle = !validator.isEmpty(params.title);
+            var validatorContent = !validator.isEmpty(params.content);
+            var articleId = req.params.id;
+
+            if(validatorTitle && validatorContent){
+                
+              
+              Aritcle.findOneAndUpdate({_id: articleId}, params, {new:true}, (err, articleUpdated) =>{
+                 
+                 if(err || !articleUpdated){
+                    return res.status(500).send({
+                        status : 'error', 
+                        message : 'No se pudo actualizar el articulo !!!'
+                      });
+                 }else{
+                    return res.status(200).send({
+                        status : 'success', 
+                        articleUpdated
+                      });
+                 }
+
+              } );
+                
+
+            }else{
+
+                return res.status(500).send({
+                    status : 'error', 
+                    message : 'La validación no es correcta !!!'
+                  });
+
+            }
+            
+        } catch (error) {
+
+            return res.status(404).send({
+                status : 'success', 
+                articles
+              });
+            
+        }
+
+    },
+
+
+
+    deleteArticle: (req, res) =>{
+
+     try {
+
+        var articleId = req.params.id;
+
+        if(articleId && articleId != null){
+
+            Article.findOneAndDelete({_id:articleId}, (err, articleDeleted) => {
+
+
+                if(err || articleDeleted){
+
+                    return res.status(200).send( {          
+                      status: 'success',
+                      message: articleDeleted
+                    });
+
+                }else{
+
+                    return res.status(500).send( {          
+                        status: 'error',
+                        message: 'No se encontró el artículo, probablemente no exista en la base de datos!!!'
+                      });
+
+                }
+
+            });
+
+        }else{
+
+            return res.status(500).send( {          
+                status: 'error',
+                message: 'No se encontró el artículo, probablemente no exista en la base de datos!!!'
+              });
+        }
+         
+     } catch (error) {
+
+        return res.status(404).send( {          
+            status: 'error',
+            message: 'No se pudo eliminar el articulo !!!'
+          });
+         
+     }
+
+    },
+
+
+  
+  upload: (req, res) => {
+
+  try {
+
+    var file_name = 'Imagen no subida';
+
+    if(!req.files){
+        return res.status(404).send( {          
+            status: 'error',
+            message: file_name
+          });
     }
+
+
+    var file_path = req.files.files0.path;
+    var file_split = file_path.file_split('\\');
+    
+    file_name = file_split[2];
+
+    var extension_split = file_name.split('\.');
+    var file_ext = extension_split[1];
+
+    if(file_ext != 'png'  && file_split.file_ext != 'jpg' && file_ext != 'jpeg' && file_ext != '.gif'){
+      
+         fs.unlink(file_path, (err) =>{
+            return res.status(200).send( {          
+                status: 'error',
+                message: 'La extensión de la imagen no es valida'
+              });
+         } );
+
+    }else{
+
+
+        //Buscar el articulo y actualizar la imágen
+        var articleId = req.params.id;
+        Article.findOneAndUpdate( {_id:articleId}, {image:file_name}, {new:true}, (err, articleUpdated) =>{
+          
+           if(err || !articleUpdated){
+            return res.status(200).send( {          
+                status: 'error',
+                message: 'Error al guardar la imagen'
+              });
+           }
+
+            return res.status(200).send( {          
+                status: 'success',
+                article: articleUpdated
+              });
+
+        });
+
+
+    }
+      
+  } catch (error) {
+
+    return res.status(404).send( {          
+        status: 'error',
+        message: 'Algo salio mal!!!'
+      });
+      
+  }
+
+
+  },
+
+
+
+  getImage: (req, res) =>{
+
+   var file = req.params.image;
+   var path_file = './upload/articles/'+file
+
+   fs.existsSync(path_file, (exists) => {
+
+      if(exists){
+
+        return res.sendFile(path.resolve(path_file));
+
+      }else{
+        return res.status(404).send( {          
+            status: 'error',
+            message: 'La imagen no existe!!!'
+          });
+      }
+
+   });
+
+  }
+
 
 
 }// end controller
--- Backend/routes/article.js
+++ Backend/routes/article.js
@@ -8,6 +8,10 @@ var ArticleController = require('../controllers/article');
 
 var router = express.Router();
 
+var multipart = require('connect-multiparty');
+var md_upload = multipart({
+    uploadDir: './upload/articles'
+})
 
 // Rutas de prueba
 router.get('/test-de_controlador', ArticleController.test);
@@ -16,6 +20,10 @@ router.post('/datos-curso', ArticleController.datosCurso);
 // Rutas útiles
 router.post('/save', ArticleController.save);
 router.get('/articles', ArticleController.getArticles);
-
+router.get('/articles:id', ArticleController.getArticles);
+router.put('/article:id', ArticleController.updateArticle);
+router.post('/article:id', ArticleController.deleteArticle);
+router.post('/article-image:id', md_upload, ArticleController.upload);
+router.get('/get-image:image',  ArticleController.getImage);
 
 module.exports = router;
